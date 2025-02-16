5248a59e244ba1e8659ef39c8b2744aedec0670a
KaiserHua1901 <133273705+KaiserHua1901@users.noreply.github.com>
2025-02-16 15:22:15 -0800

    Adding Example Item into the game with name and texture

--- src/main/java/net/kaiser/tutorialmod/TutorialMod.java
+++ src/main/java/net/kaiser/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.kaiser.tutorialmod;
 
 import com.mojang.logging.LogUtils;
 import net.kaiser.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -36,7 +37,12 @@ public class TutorialMod
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
         ModItems.register(modEventBus);
+
+
+
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -50,7 +56,10 @@ public class TutorialMod
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.EXAMPLEITEM);
+            // This will add item into creative mode tab Ingredients
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/net/kaiser/tutorialmod/item/ModItems.java
+++ src/main/java/net/kaiser/tutorialmod/item/ModItems.java
@@ -1,14 +1,21 @@
 package net.kaiser.tutorialmod.item;
 
 import net.kaiser.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TutorialMod.MODID);
+    // Register a new item FROM the MODID to Items class. that will be inside MODID
 
-    public static void register (IEventBus eventBus){
+    public static final DeferredItem<Item> EXAMPLEITEM = ITEMS.register("exampleitem",
+            () -> new Item(new Item.Properties()));
+    // I THINK This will add item into the game. but without asset or even in creative tab or anywhere.
+
+    public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
+        // No clue what this does yet.
     }
-
 }
